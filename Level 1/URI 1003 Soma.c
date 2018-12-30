99eb6e63baee0388b63909d7223fde034bdff47a
Tiago Januario <januarioccp@gmail.com>
2018-12-30 15:37:10 -0300

    Simple sum solution

---
+++ Level 1/URI 1003 Soma.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ 
+    int A;
+    int B;
+    scanf("%d",&A);
+    scanf("%d",&B);
+    printf("SOMA = %d\n",A+B);
+    return 0;
+}
