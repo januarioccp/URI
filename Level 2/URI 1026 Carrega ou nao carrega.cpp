83bff70ab45b9049d8eb43984bb8a0d08c844523
Tiago Januario <januarioccp@gmail.com>
2018-12-30 23:53:44 -0300

    rename files

--- Level 2/URI 1026 Carrega ou nao carrega.cpp
+++ Level 2/URI 1026 Carrega ou nao carrega.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
 
     // input data
-    long int a,b;
-    long int resp;
+    unsigned long int a,b;
+    unsigned long int resp;
 
     while(cin>>a>>b){
         resp = a^b;//XOR
