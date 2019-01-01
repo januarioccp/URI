2758e52e8ce2c56549dd0267482df73de1c0d237
Tiago Januario <januarioccp@gmail.com>
2019-01-01 11:37:51 -0300

    Create 1005 - Média 1.cpp

---
+++ Level 1/1005 - Média 1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     double a,b;
+     scanf("%lf",&a);
+     scanf("%lf",&b);
+     printf("MEDIA = %.5lf\n",(3.5*a+7.5*b)/11);
+ 
+    return 0;
+}
