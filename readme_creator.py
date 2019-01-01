80a7ba640e8854a1f33a35b34849a177f695ebee
Tiago Januario <januarioccp@gmail.com>
2019-01-01 20:37:30 -0300

    improved version of readme creator

---
+++ readme_creator.py
@@ -0,0 +1,23 @@
+import os
+# Take the list of all subfolders
+subfolders = [f.path for f in os.scandir('.') if f.is_dir() ]
+subfolders.sort()
+# Iterare through all subfolders
+for directory in subfolders:
+    # Check if the subfolder is not hidden 
+    if(directory[2]!='.'):
+        # Open a README file in this subfolder
+        readme=open(directory+'/README.md', 'w+')
+        print (directory+'/README.md')
+        # Read all files in this subfolder
+        files = os.listdir(directory+'/')
+        # Sort all files
+        files.sort()
+        # write the necessary information in the README file
+        readme.write("# Link to each Problem Description\n")
+        for solution in files:
+            if(solution[0]=='r') or (solution[0]=='R'):
+                continue
+            else:
+                readme.write("* ["+solution[:-4]+"](https://www.urionlinejudge.com.br/judge/pt/problems/view/"+solution[:4]+")\n")
+        readme.close()
