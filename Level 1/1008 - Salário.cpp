55511d3ea96edef55ec3fc05f3c567429d32d3bd
Tiago Januario <januarioccp@gmail.com>
2019-01-07 06:28:16 -0300

    Create 1008 - Salário.cpp

---
+++ Level 1/1008 - Salário.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+	int n;
+     double h,v;
+     scanf("%d",&n);
+     scanf("%lf",&h);
+     scanf("%lf",&v);
+     printf("NUMBER = %d\n",n);
+     printf("SALARY = U$ %.2lf\n",h*v);
+ 
+    return 0;
+}
