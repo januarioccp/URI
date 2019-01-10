e4661acea91d6ccf894c6f4533908cb6ba58e67b
Tiago Januario <januarioccp@gmail.com>
2019-01-09 22:19:42 -0300

    Reorganization

---
+++ Selection/1048 - Aumento de Salário.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    double salario;
+     cin>>sax   cin>>b;
+     cin>>c;
+     cin>>d;
+     cout<<"DIFERENCA = "<<a*b-c*d<<endl;
+ 
+    return 0;
+}
