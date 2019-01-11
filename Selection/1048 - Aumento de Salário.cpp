afe826f383d401e0cbb8a230f1e465ed70ca6b2d
Tiago Januario <januarioccp@gmail.com>
2019-01-11 17:57:44 -0200

    Update 1048 - Aumento de Salário.cpp

--- Selection/1048 - Aumento de Salário.cpp
+++ Selection/1048 - Aumento de Salário.cpp
@@ -5,10 +5,36 @@ using namespace std;
 int main() {
  
     double salario;
-     cin>>sax   cin>>b;
-     cin>>c;
-     cin>>d;
-     cout<<"DIFERENCA = "<<a*b-c*d<<endl;
+    double novo_salario;
+    double reajuste;
+    double percentual;
+    
+    cin>>salario;
  
+    if (salario > 2000.0){
+        percentual = 4.0;        
+        novo_salario = salario*(1+percentual/100);
+        reajuste = novo_salario - salario;       
+    } else if (salario > 1200.0){
+        percentual = 7.0;        
+        novo_salario = salario*(1+percentual/100);
+        reajuste = novo_salario - salario;       
+    } else if (salario > 800.0){
+        percentual = 10.0;        
+        novo_salario = salario*(1+percentual/100);
+        reajuste = novo_salario - salario;       
+    } else if (salario > 400.0){
+        percentual = 12.0;        
+        novo_salario = salario*(1+percentual/100);
+        reajuste = novo_salario - salario;       
+    } else if (salario >= 0.0){
+        percentual = 15.0;        
+        novo_salario = salario*(1+percentual/100);
+        reajuste = novo_salario - salario;       
+    }
+    printf("Novo salario: %.2f\n",novo_salario);
+    printf("Reajuste ganho: %.2f\n",reajuste);
+    printf("Em percentual: %.0f \%\n",percentual);
+    
     return 0;
 }
