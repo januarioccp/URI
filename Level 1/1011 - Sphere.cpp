934eb77c55681f5bb4e6696bc02065476994e89d
Tiago Januario <januarioccp@gmail.com>
2019-01-13 14:29:00 -0300

    Create 1011 - Sphere.cpp

---
+++ Level 1/1011 - Sphere.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     
+     //int id1, id2;
+     //double t1,t2;
+     //double v1,v2;
+     double r;//s,b;
+     scanf("%lf",&r);
+     //scanf("%d %lf %lf",&id2,&t2,&v2);
+     printf("VOLUME = %.3lf\n",(4.0/3.0)*3.14159*r*r*r);
+     
+    return 0;
+}
