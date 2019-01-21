26322db4c3f7bd6a73681cc4645ddf40bf26cdb9
Tiago Januario <januarioccp@gmail.com>
2019-01-21 20:14:18 -0300

    Create 1016 - Distance.cpp

---
+++ Level 1/1016 - Distance.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int n;
+    //double d;
+    scanf("%d",&n);
+    //scanf("%lf",&d);
+    printf("%d minutos\n",2*n);
+        
+}
