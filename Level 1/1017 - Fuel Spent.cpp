df0294817a254cd658fea9bc6fd8c20860d38e2d
Tiago Januario <januarioccp@gmail.com>
2019-01-21 20:14:58 -0300

    Create 1017 - Fuel Spent.cpp

---
+++ Level 1/1017 - Fuel Spent.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    //int n;
+    int a,b;
+    //double d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    //scanf("%lf",&d);
+    printf("%.3lf\n",1.0*a*1.0*b/12.0);
+        
+}
